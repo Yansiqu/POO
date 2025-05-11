2f4e60dd175c5a29d4c87489355874dc5868828c
Yan rodrigues <166083851+Yansiqu@users.noreply.github.com>
2025-05-11 12:09:26 -0300

    Add files via upload

--- ProgramaAnimal.java
+++ ProgramaAnimal.java
@@ -1,38 +1,11 @@
 public class ProgramaAnimal {
-
-    // Classe base Animal
-    public static class Animal {
-        String nome;
-
-        public void emitirSom() {
-            System.out.println("O animal faz um som.");
-        }
-    }
-
-    // Classe derivada Gato
-    public static class Gato extends Animal {
-        @Override
-        public void emitirSom() {
-            System.out.println("O gato mia: Miau!");
-        }
-    }
-
-    // Classe derivada Cachorro
-    public static class Cachorro extends Animal {
-        @Override
-        public void emitirSom() {
-            System.out.println("O cachorro late: Au au!");
-        }
-    }
-
-    // Método principal
     public static void main(String[] args) {
-        Animal a = new Animal();
-        Gato g = new Gato();
-        Cachorro c = new Cachorro();
+        Animal animal = new Animal();
+        Gato gato = new Gato();
+        Cachorro cachorro = new Cachorro();
 
-        a.emitirSom();     // O animal faz um som.
-        g.emitirSom();     // O gato mia: Miau!
-        c.emitirSom();     // O cachorro late: Au au!
+        animal.emitirSom();    
+        gato.emitirSom();      
+        cachorro.emitirSom();  
     }
 }
